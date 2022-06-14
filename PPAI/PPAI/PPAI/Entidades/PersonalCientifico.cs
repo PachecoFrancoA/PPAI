d228b2e4638bf7f0511a1b9f8d05df8162fe3bd3
Albano Zupichiatti <89983599+Albano21@users.noreply.github.com>
2022-06-14 20:04:38 -0300

    personalCientifico tipoRT

--- PPAI/PPAI/PPAI/Entidades/PersonalCientifico.cs
+++ PPAI/PPAI/PPAI/Entidades/PersonalCientifico.cs
@@ -42,6 +42,46 @@ namespace PPAI.Entidades
             return this.legajo;
         }
 
+        public void setNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public string getNombre()
+        {
+            return this.nombre;
+        }
+
+        public void setCorreo_inst(string correo_inst)
+        {
+            this.correo_inst = correo_inst;
+        }
+
+        public string getCorreo_inst()
+        {
+            return this.correo_inst;
+        }
+
+        public void setCorreo_pers(string correo_pers)
+        {
+            this.correo_pers = correo_pers;
+        }
+
+        public string getCorreo_pers()
+        {
+            return this.correo_pers;
+        }
+
+        public void setUsuario(Usuario usuario)
+        {
+            this.usuario = usuario;
+        }
+
+        public Usuario getUsuario()
+        {
+            return this.usuario;
+        }
+
 
         public Boolean esTuUsuario(Usuario usuario)
         {
--- PPAI/PPAI/PPAI/Entidades/TipoRT.cs
+++ PPAI/PPAI/PPAI/Entidades/TipoRT.cs
@@ -8,12 +8,37 @@ namespace PPAI.Entidades
 {
     public class TipoRT
     {
-        public string nombre { get; set; }
-        public string descripcion { get; set; }
+        private string nombre { get; set; }
+        private string descripcion { get; set; }
+
+        public TipoRT(string nombre, string descripcion)
+        {
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+        }
+
+        public TipoRT()
+        {
+        }
+
+        public void setNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
 
         public String getNombre()
         {
             return this.nombre;
+        }
+
+        public void setDescripcion(string descripcion)
+        {
+            this.descripcion = descripcion;
+        }
+
+        public string getDescripcion()
+        {
+            return this.descripcion;
         }
     }
 }
